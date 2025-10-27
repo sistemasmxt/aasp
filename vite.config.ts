9f5b303c547c34d94d15ca4dca9cb212f18bfbbb
Thabata Bastos <thabata@example.com>
2025-10-27 16:47:13 -0300

    Desabilitando PWA em produção para evitar erro de build

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === "development" && componentTagger(),
-    VitePWA({
+    mode === "production" && VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       manifest: {
