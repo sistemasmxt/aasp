8c0aeac8bf7bdcc2f904a7995706f0f80eed01c1
Thabata Bastos <thabata@example.com>
2025-11-05 19:58:39 -0300

    Backup commit: update project files including deleted deploy.bat and modified sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-f03bcc19'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.n63u9vcbsfo"
+    "revision": "0.kbksem0if1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
