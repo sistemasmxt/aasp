c2888e9dd98f5caffdacf35fc09dce25afd2fd71
[dyad] <sistemasmxt@gmail.com>
2025-12-01 16:40:34 -0300

    [dyad] Fixing Supabase connection error - wrote 1 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-f03bcc19'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.875sh4ahtvg"
+    "revision": "0.7qh0vajl088"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
